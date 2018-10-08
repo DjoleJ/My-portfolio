4216ce3bdb2de939095d715a410bb402b8726f98
Djorjde Jordanovic <djole.jordanovic@gmail.com>
2018-10-08 20:00:00 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,39 @@
+(function() {
+    
+
+var html5String = 'Knowledge of HTML5, its modern concepts and semantic structure.';
+var css3String = 'Knowledge of CSS3, responsive design and its modern concepts like CSS Grid, Flexbox, animations and much more.';
+var javascriptString = 'Knowledge of Javascript and modern technologies like ES6, npm, webpack.';
+var jqueryString = 'Knowledge of the JavaScript library jQuery and its core DOM manipulation functionalities.';
+var bootstrapString = 'Knowledge of the front-end framework Bootstrap and its core functionalities like Grid System, responsive design, modal windows, carousels and much more.';
+var sassString = 'Knowledge of CSS preprocessors SCSS and SASS, and its advantages like nesting, variables, mixins, functions and more.';
+var gitString = 'Knowledge of version control system Git and GitHub, and its basic commands like adding, commiting, branching and pushing files to a remote repository.';
+var reactString = 'Basic knowledge of JavaScript framework React, and its components, structures, methods and workflow.';
+
+
+
+var strings = [html5String, css3String, javascriptString, jqueryString, bootstrapString, sassString, gitString, reactString]
+
+
+    
+document.getElementById('skills').addEventListener('click', function(e) {
+    
+    var target = e.target;
+    
+    for(var i = 0; i < 8; i++) {
+        
+        if(target.id == ('skill-box-'+(i+1)) || target.parentNode.id == ('skill-box-'+(i+1))) {
+            
+            if(document.getElementById('skill-box-p-'+(i+1)).textContent == '' ) {
+                document.getElementById('skill-box-p-'+(i+1)).textContent = strings[i];
+            } else {
+                document.getElementById('skill-box-p-'+(i+1)).textContent = '';
+            }
+        } 
+    }
+    
+    
+    
+});
+    
+})();
